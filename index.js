14ebada7ba9446c569b9d53f840808d485ffe938
Victor Petrovych <victor.petrovych@teamvoy.com>
2021-12-12 14:19:57 +0200

    Add save img logic

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const WSServer = require('express-ws')(app)
 const aWss = WSServer.getWss()
+const cors = require('cors')
+const fs = require('fs')
+const path = require("path");
+
 const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
 
 function broadcastConnection(ws, msg) {
     console.log(
@@ -37,4 +44,24 @@ app.ws('/', (ws, res) => {
     })
 })
 
+app.post('/image', (req, res) => {
+    try {
+        const data = req.body.img.replace('data:image/png;base64,', '')
+        fs.writeFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`), data, 'base64')
+        return res.status(200).json({message: 'OK'})
+    } catch (e) {
+        console.log('ERROR', e)
+        return res.status(500).json('ERROR')
+    }
+})
+app.get('/image', (req, res) => {
+    try {
+        const file = fs.readFileSync(path.resolve(__dirname, 'files', `${req.query.id}.jpg`))
+        const data = 'data:image/png;base64,' + file.toString('base64')
+        return res.status(200).json(data)
+    } catch (e) {
+        console.log('ERROR', e)
+        return res.status(500).json('ERROR')
+    }
+})
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
